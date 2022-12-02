4f3cea626824992a2dd619c8602ce2b3c55c99f8
Kebe <kebe.liu@daocloud.io>
2022-12-02 12:16:05 +0800

    > fix monitor

--- process_watcher.go
+++ process_watcher.go
@@ -49,18 +49,6 @@ func NewProcessWatcher() Watcher {
 	}
 }
 
-func iovecs2bytes(iovecs []unix.Iovec) [][]byte {
-	bs := make([][]byte, len(iovecs))
-	for i, v := range iovecs {
-		b := make([]byte, v.Len)
-		for j := 0; j < int(v.Len); j++ {
-			b[j] = *(*byte)(unsafe.Pointer(uintptr(unsafe.Pointer(v.Base)) + uintptr(j)))
-		}
-		bs[i] = b
-	}
-	return bs
-}
-
 func (pw *processWatcher) Stop() {
 	syscall.Close(pw.sock)
 	close(pw.stop)
@@ -80,6 +68,15 @@ func (pw *processWatcher) sendEvents(e WatchEvent) {
 	pw.events <- e
 }
 
+type msg_data struct {
+	cn_msg
+	cn_mcast uint32
+}
+type processMonitorMsg struct {
+	nl_hdr syscall.NlMsghdr
+	data   msg_data
+}
+
 func (pw *processWatcher) Start() error {
 	sock, err := syscall.Socket(syscall.AF_NETLINK, syscall.SOCK_DGRAM, syscall.NETLINK_CONNECTOR)
 	if err != nil {
@@ -89,31 +86,38 @@ func (pw *processWatcher) Start() error {
 	if err := syscall.Bind(sock, addr); err != nil {
 		return err
 	}
-	var op uint32 = 1
 	headerSize := uint64(unsafe.Sizeof(syscall.NlMsghdr{}))
-	opSize := uint64(unsafe.Sizeof(op))
 	msgSize := uint64(unsafe.Sizeof(cn_msg{}))
-	var iovecs = make([]unix.Iovec, 3)
-	s := uint32(msgSize + opSize + headerSize)
-	h := syscall.NlMsghdr{
-		Len:  s,
-		Pid:  uint32(os.Getegid()),
-		Type: syscall.NLMSG_DONE,
-	}
-	iovecs[0].Base = (*byte)(unsafe.Pointer(&h))
-	iovecs[0].Len = headerSize
-	m := cn_msg{
-		id: cb_id{
-			idx: CN_IDX_PROC,
-			val: CN_VAL_PROC,
+	mm := processMonitorMsg{
+		nl_hdr: syscall.NlMsghdr{
+			Len:   uint32(unsafe.Sizeof(processMonitorMsg{})),
+			Type:  syscall.NLMSG_DONE,
+			Flags: 0,
+			Pid:   uint32(os.Getegid()),
 		},
-		len: uint16(opSize),
+		data: msg_data{
+			cn_msg: cn_msg{
+				id: cb_id{
+					idx: CN_IDX_PROC,
+					val: CN_VAL_PROC,
+				},
+				len: 4,
+			},
+			cn_mcast: 1,
+		},
 	}
-	iovecs[1].Base = (*byte)(unsafe.Pointer(&m))
-	iovecs[1].Len = msgSize
-	iovecs[2].Base = (*byte)(unsafe.Pointer(&op))
-	iovecs[2].Len = opSize
-	_, err = unix.Writev(sock, iovecs2bytes(iovecs))
+	type slice struct {
+		p   uintptr
+		len uint64
+		cap uint64
+	}
+	x := slice{
+		p:   uintptr(unsafe.Pointer(&mm)),
+		len: uint64(unsafe.Sizeof(processMonitorMsg{})),
+		cap: uint64(unsafe.Sizeof(processMonitorMsg{})),
+	}
+	mmm := *(*[]byte)(unsafe.Pointer(&x))
+	err = unix.Send(sock, mmm, 0)
 	if err != nil {
 		return err
 	}
